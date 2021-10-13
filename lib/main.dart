68b1d94d418ae667af0d82389fa0d4f30b67b902
Cicero <equation0@outlook.es>
2021-10-13 14:01:06 -0500

    reto de platzi curo de Anahi

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import "package:flutter/material.dart";
 import 'package:flutter_application_2/home/home.page.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
